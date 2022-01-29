8202ca53ce927a51b86ad0750048022173873c9c
naamp <naamp@LAPTOP-43DO08TM>
2022-01-29 08:20:07 -0500

    Add project files.

---
+++ GameCanvas.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battleship_game
+{
+    public class GameCanvas
+    {
+        const int x_GridSize = 10;
+        const int y_GridSize = 10;
+        const int tableWidth2= 40;
+        const int width = (tableWidth2 - x_GridSize) / x_GridSize;
+
+        static void PrintRow(params string[] columns)
+        {
+            string row = string.Empty;
+
+            foreach (string column in columns)
+            {
+                row += AlignCentre2(column, width);
+            }
+
+            Console.WriteLine(row);
+        }
+
+        public void createBattleshipGameCanvas()
+        {
+            PrintLine();
+            PrintRow("", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+
+            for (int y = 0; y < y_GridSize; y++)
+            {
+                Console.Write($"{ y + 1}".PadLeft(width));
+
+                for (int x = 0; x < x_GridSize; x++)
+                {
+                    string row = AlignCentre2("-", width);
+                    Console.Write(row);                  
+                }
+                Console.WriteLine();
+            }
+            PrintLine();
+        }
+
+        private static string AlignCentre2(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+        static void PrintLine()
+        {
+            Console.WriteLine(new string('-', tableWidth2));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Battleship_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userTurn = 10;
+            int battleShip_lives = 5;
+     
+            //while guess is more than 0 and game is not over
+            while (userTurn > 0 && battleShip_lives > 0)
+            {
+                //execute this
+                GameCanvas gameCanvas = new GameCanvas();
+                gameCanvas.createBattleshipGameCanvas();
+
+                Console.WriteLine("User turn remaining: {0}", userTurn);
+                Console.WriteLine("Battleship lives remaining: {0}", battleShip_lives);
+                Console.WriteLine("Please enter your next move:");
+
+                //Todo: randomly place the ship
+
+
+                bool inputComplete = false;
+                while (!inputComplete) //while this is true
+                {
+                    try
+                    {
+                        //get user input && determine if the input is valid
+                        InputConverter inputConverter = new InputConverter();
+                        int num1 = inputConverter.convertInput(Console.ReadLine());
+                        inputComplete = true;
+
+                        //user input --1
+                        userTurn--;
+
+                    } catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+                //Todo: determine if the ship has been hit.
+
+                
+            }
+
+        }
+
+
+
+
+    }
+}
